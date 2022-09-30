5f5e6773185ae88d75a27cb3f8f86e85344643ae
JuKoNi <92727651+JuKoNi@users.noreply.github.com>
2022-09-30 22:41:51 +0200

    Update constant.tsx

--- frontend/src/models/constant.tsx
+++ frontend/src/models/constant.tsx
@@ -1,2 +1,2 @@
-export const API_URL:string = 'https://rewind-exam-api.onrender.com/';
+export const API_URL:string = 'https://rewind-exam-api.onrender.com';
 //export const API_URL:string = 'http://backend:3000';
